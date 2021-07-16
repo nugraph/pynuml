726517c97d5cc68f69f22a2b1c0a137bf5f6aede
jeremy hewes <jhewes15@fnal.gov>
2021-07-16 12:00:35 -0400

    updates to graph processing
    added additional edge labelling scheme to graphs, and natively store them as torch geometric Data objects rather than a dictionary

--- numl/process/hitgraph.py
+++ numl/process/hitgraph.py
@@ -1,6 +1,5 @@
-import pandas as pd, torch
+import pandas as pd, torch, torch_geometric as tg
 from ..core.file import NuMLFile
-from ..core.out import H5Out
 from ..labels import *
 from ..graph import *
 
@@ -42,13 +41,13 @@ def single_plane_graph(out, key, hit, part, edep, l=ccqe, e=edges.window_edges):
     graph_dict = {
       "x": torch.tensor(plane[node_feats].to_numpy()).float(),
       "edge_index": torch.tensor(edge[["idx_1", "idx_2"]].to_numpy().T).long(),
-      "y": torch.tensor(plane["label"].to_numpy()).long()
+      "y": torch.tensor(plane["label"].to_numpy()).long(),
+      "y_edge": torch.tensor(edge["label"].to_numpy()).long()  
     }
-    out.save(graph_dict, f"r{key[0]}_sr{key[1]}_evt{key[2]}_p{p}")
+    out.save(tg.data.Data(**graph_dict), f"r{key[0]}_sr{key[1]}_evt{key[2]}_p{p}")
 
 def process_file(out, fname, g=single_plane_graph, l=ccqe, e=edges.window_edges):
   """Process all events in a file into graphs"""
-  print("change successful")
   f = NuMLFile(fname)
 
   evt = f.get_dataframe("event_table", ["event_id"])
