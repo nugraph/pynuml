1c89fcf2317a51f82eeb08f81118434a9adfe367
jeremy hewes <jhewes15@fnal.gov>
2021-07-22 10:56:27 -0400

    add graph node drawing function

--- numl/plot/graph.py
+++ numl/plot/graph.py
@@ -1,13 +1,20 @@
-import torch, matplotlib as mpl
+import torch, matplotlib as mpl, seaborn as sns, numpy as np
 import matplotlib.pyplot as plt, matplotlib.collections as mc
 
-def _format():
-  """Default plot formatting"""
+def _init():
+  """Default font formatting"""
   mpl.rc('font', weight='bold', size=36)
+  return plt.subplots(figsize=[16,9])
+
+def _format(ax):
+  """Default plot formatting"""
+  ax.autoscale()
+  plt.xlabel("Wire")
+  plt.ylabel("Time tick")
+  plt.tight_layout()
 
 def _get_lines(g, score):
   """Take a g object and return a list of LineCollection objects, one per class"""
-  _format()
   # wire = g.x[:,1]
   # time = g.x[:,2]
   wire = g["x"][:,1]
@@ -19,23 +26,25 @@ def _get_lines(g, score):
   for l, y in zip(lines, score): lines_class[y].append(l)
   return [ mc.LineCollection(lines_class[i], colors=colours[i], linewidths=2, zorder=1) for i in range(len(colours)) ]
 
+def plot_node_score(g, y):
+  """Plot graph nodes, colour-coded by node label"""
+  fig, ax = _init()
+  c = np.array(sns.color_palette())[y]
+  plt.scatter(g["x"][:,1], g["x"][:,2], c=c, s=8)
+  _format(ax)
+
 def plot_edge_score(g, y):
   """Plot graph edges, colour-coded by edge score"""
-  _format()
-  fig, ax = plt.subplots(figsize=[16,9])
+  fig, ax = _init()
   lcs = _get_lines(g, y)
   for lc in lcs: ax.add_collection(lc)
-  ax.autoscale()
-  plt.xlabel("Wire")
-  plt.ylabel("Time tick")
-  plt.tight_layout()
+  _format(ax)
 
 def plot_edge_diff(g, y):
   """Plot graph edges, highlighting edges that were misclassified"""
-  _format()
-  fig, ax = plt.subplots(figsize=[16,9])
+  fig, ax = _init()
   y = (y != g.y)
   lcs = _get_lines(g, y)
   for lc in lcs: ax.add_collection(lc)
-  ax.autoscale()
-  plt.tight_layout()
+  _format(ax)
+
