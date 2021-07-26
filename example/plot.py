803b200c51b3187eb6a95cb0e2f88cf21a2d8475
jeremy hewes <jhewes15@fnal.gov>
2021-07-26 16:23:13 -0400

    update example graph plotting script

--- example/plot.py
+++ example/plot.py
@@ -1,11 +1,18 @@
-import sys, os.path as osp
-sys.path.append(osp.dirname(osp.dirname(osp.realpath(__file__))))
-import numl, glob, torch, matplotlib.pyplot as plt
 
-graphs = glob.glob("*.pt")
+if __name__ == "__main__":
+  import sys, os.path as osp
+  sys.path.append(osp.dirname(osp.dirname(osp.realpath(__file__))))
+  import numl, glob, torch, matplotlib.pyplot as plt
 
-for graph in graphs:
-  data = torch.load(graph)
-  numl.plot.graph.plot_edge_score(data, data["y"])
-  plt.savefig(osp.splitext(graph)[0]+".png")
-  plt.clf()
+  graphs = glob.glob(sys.argv[1]+"/*")
+
+  for graph in graphs:
+    print(f"plotting graph {osp.basename(graph)}")
+    data = torch.load(graph)
+    name = osp.splitext(osp.basename(graph))[0]
+    numl.plot.graph.plot_edge_score(data, data["y_edge"])
+    plt.savefig(name+"_edge.png")
+    plt.close()
+    numl.plot.graph.plot_node_score(data, data["y"])
+    plt.savefig(name+"_node.png")
+    plt.close()
