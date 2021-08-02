a800dac3aa062f34dbd81f9e7610cd0b88405c42
jeremy hewes <jhewes15@fnal.gov>
2021-08-02 09:15:54 -0400

    adding generator
    adding python generator that loops over events and slices out the dataframe from that event from the larger table, instead of loading the entire file into memory first. this is probably more memory efficient, but slow for now, and hard to parallelise over

--- numl/core/file.py
+++ numl/core/file.py
@@ -45,4 +45,23 @@ class NuMLFile:
 
   def get_dataframe(self, group, keys=[]):
     if not keys: keys = list(self._file[group].keys())
-    return pd.concat([ pd.DataFrame(np.array(self._file[group][key]), columns=self._cols(group, key)) for key in keys ], axis=1).set_index(["run","subrun","event"])
+    dfs = [ pd.DataFrame(np.array(self._file[group][key]), columns=self._cols(group, key)) for key in keys ]
+    return pd.concat(dfs, axis="columns").set_index(["run","subrun","event"])
+
+  def events(self, groups):
+    for i in range(len(self)):
+      index = self.id(i)
+      ret = {}
+      # loop over groups in file
+      for group, datasets in groups:
+        # mask out based on run, subrun and event
+        m = (self._file[group]["event_id"][()] == index).all(axis=1)
+        if not datasets: datasets = list(self._file[group].keys())
+        def slice(g, d, m):
+          n = self._file[g][d].shape[1]
+          m = m[:,None].repeat(n, axis=1)
+          return pd.DataFrame(self._file[g][d][m].reshape([-1,n]), columns=self._cols(g,d))
+        dfs = [ slice(group, dataset, m) for dataset in datasets ]
+        ret[group] = pd.concat(dfs, axis="columns")
+      yield ret
+
