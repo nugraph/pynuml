d61dc645712110d7dc5c34ced49fd49dafffdf57
jeremy hewes <jhewes15@fnal.gov>
2021-08-02 09:18:55 -0400

    update graph processing
    some changes to the processing function, to accommodate both new graph edge schemes and attempts at better multiprocessing. neither is fully functional just yet

--- numl/process/hitgraph.py
+++ numl/process/hitgraph.py
@@ -3,26 +3,35 @@ from ..core.file import NuMLFile
 from ..labels import *
 from ..graph import *
 
-def single_plane_graph(out, key, hit, part, edep, l=ccqe, e=edges.delaunay):
+def single_plane_graph(gen, l=ccqe.hit_label, e=edges.delaunay):
   """Process an event into graphs"""
-  # skip any events with no simulated hits
-  if (hit.index==key).sum() == 0: return
-  if (edep.index==key).sum() == 0: return
 
-  # get energy depositions, find max contributing particle, and ignore any hits with no truth
-  evt_edep = edep.loc[key].reset_index(drop=True)
-  evt_edep = evt_edep.loc[evt_edep.groupby("hit_id")["energy_fraction"].idxmax()]
-  evt_hit = evt_edep.merge(hit.loc[key].reset_index(), on="hit_id", how="inner").drop("energy_fraction", axis=1)
+  evt = next(gen)
+
+  key = evt["event_table"].values
+  print(key)
+
+  import os.path as osp
+  if osp.exists(f"/raid/uboone/pandora/processed_delaunay/r{key[0]}_sr{key[1]}_evt{key[2]}_p0.pt"):
+    print(f"skipping {key}")
+    return
+
+  # skip any events with no simulated hits
+  # if (hit.index==key).sum() == 0: return
+  # if (edep.index==key).sum() == 0: return
+
+  # get energy depositions, find max contributing particle, and ignore any evt_hits with no truth
+  evt_edep = evt["edep_table"].loc[evt_edep.groupby("hit_id")["energy_fraction"].idxmax()]
+  evt_hit = evt_edep.merge(evt["hit_table"], on="hit_id", how="inner").drop("energy_fraction", axis=1)
 
   # skip events with fewer than 50 simulated hits in any plane
   for i in range(3):
     if (evt_hit.global_plane==i).sum() < 50: return
 
-  # get labels for each particle
-  evt_part = part.loc[key].reset_index(drop=True)
-  evt_part = l.hit_label(evt_part)
+  # get labels for each evt_particle
+  evt_part = l(evt["part_table"])
 
-  # join the dataframes to transform particle labels into hit labels
+  # join the dataframes to transform evt_particle labels into hit labels
   evt_hit = evt_hit.merge(evt_part.drop(["parent_id", "type"], axis=1), on="g4_id", how="inner")
 
   # draw graph edges
@@ -31,33 +40,56 @@ def single_plane_graph(out, key, hit, part, edep, l=ccqe, e=edges.delaunay):
     # Reset indices
     plane = plane.reset_index(drop=True).reset_index()
 
-    # Build and label edges
-    edge = e(plane)
-    edge = l.edge_label(edge)
-
-    # Save to file
+    pos = plane[["global_wire", "global_time"]].values / torch.tensor([0.5, 0.075])[None, :].float()
     node_feats = ["global_plane", "global_wire", "global_time", "tpc",
       "local_plane", "local_wire", "local_time", "integral", "rms"]
-    graph_dict = {
-      "x": torch.tensor(plane[node_feats].to_numpy()).float(),
-      "edge_index": torch.tensor(edge[["idx_1", "idx_2"]].to_numpy().T).long(),
-      "y": torch.tensor(plane["label"].to_numpy()).long(),
-      "y_edge": torch.tensor(edge["label"].to_numpy()).long()  
-    }
-    out.save(tg.data.Data(**graph_dict), f"r{key[0]}_sr{key[1]}_evt{key[2]}_p{p}")
+    data = tg.data.Data(
+      x=torch.tensor(plane[node_feats].values).float(),
+      y=torch.tensor(plane["label"].values).long(),
+      pos=pos,
+    )
+    data = e(data)
+    print(data)
+    out.save(data, f"r{key[0]}_sr{key[1]}_evt{key[2]}_p{p}")
 
-def process_file(out, fname, g=single_plane_graph, l=ccqe, e=edges.delaunay):
-  print("Processing ", fname)
+def process_file(out, fname, g=single_plane_graph, l=ccqe.hit_label, e=edges.delaunay, p=None):
   """Process all events in a file into graphs"""
+  print(f"Processing {fname}")
   f = NuMLFile(fname)
 
-  evt = f.get_dataframe("event_table", ["event_id"])
-  hit = f.get_dataframe("hit_table")
-  part = f.get_dataframe("particle_table", ["event_id", "g4_id", "parent_id", "type"])
-  edep = f.get_dataframe("edep_table")
+  groups = [
+    ["event_table", ["event_id"]],
+    ["hit_table", []],
+    ["particle_table", ["event_id", "g4_id", "parent_id", "type"]],
+    ["edep_table", []],
+  ]
 
-  # loop over events in file
-  for key in evt.index: g(out, key, hit, part, edep, l, e)
+  gen = f.events(groups)
+
+  if p is None:
+    for i in range(len(f)):
+      name, data = g(gen, l, e)
+      out.save(data, name)
+  else:
+    from functools import partial
+    import multiprocessing as mp
+    procs = [ None for i in range(p) ]
+    c = 0
+    with mp.Pool(processes=p) as pool:
+      while True:
+        for i in range(p):
+          if procs[i] is None:
+            procs[i] = pool.apply_async(g, (gen, l, e))
+            c += 1
+          elif procs[i].ready():
+            data = procs[i].get()
+            out.save(data, name)
+            procs[i] = pool.apply_async(g, (gen, l, e))
+            c += 1
+          if c == len(f): return
+
+    # func = partial(g, out=out, l=l, e=e)
+    # with mp.Pool(processes=p) as pool: pool.map(func, gen)
 
   print('End processing ', fname)
 
