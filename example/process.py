2404c3ab438aa37310dcbd2e9a348da16ec7d062
wkliao <wkliao@users.noreply.github.com>
2021-09-07 12:48:56 -0500

    use proper file name

--- example/process.py
+++ example/process.py
@@ -4,5 +4,5 @@ import numl, glob
 
 out = numl.core.out.PTOut("/files2/scratch/wkliao/Fermi/output")
 # numl.process.hitgraph.process_file(out, "/files2/scratch/wkliao/Fermi/x1_key_off_len.h5")
-numl.process.hitgraph.process_file(out, "/files2/scratch/wkliao/Fermi/x0123_key_off_len.h5")
+numl.process.hitgraph.process_file(out, "/files2/scratch/wkliao/Fermi/x0123_seq_cnt.h5")
 
