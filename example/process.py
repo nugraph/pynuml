13b6e20616110614e306ae1a736c46b89cdabd58
wkliao <wkliao@users.noreply.github.com>
2021-09-09 16:57:03 -0500

    add command-line arguments

--- example/process.py
+++ example/process.py
@@ -1,12 +1,43 @@
 import sys, os.path as osp
 sys.path.append(osp.dirname(osp.dirname(osp.realpath(__file__))))
 import numl, glob
+import getopt
 
-# output pytorch files, one graph per file
-out = numl.core.out.PTOut("/files2/scratch/wkliao/Fermi/output")
+def main(argv):
+  profiling = False
+  use_seq = False
+  inputfile = ""
+  outputfile = ""
+  try:
+    opts, args = getopt.getopt(argv,"hpsi:o:",["ifile=","ofile="])
+  except getopt.GetoptError:
+    print("Usage: process.py [-p|-s] -i <inputfile> -o <outputfile>")
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == "-h":
+      print("Usage: process.py [-p|-s] -i <inputfile> -o <outputfile>")
+      sys.exit()
+    elif opt in ("-i", "--ifile"):
+      inputfile = arg
+    elif opt in ("-o", "--ofile"):
+      outputfile = arg
+    elif opt == "-p":
+      profiling = True
+    elif opt == "-s":
+      use_seq = True
 
-# output HDF5 files, one graph per group
-# TODO: use parallel HDF5
-# out = numl.core.out.H5Out("/files2/scratch/wkliao/Fermi/output/x0123_out.h5")
+  if inputfile == "" or outputfile == "":
+    print("Usage: process.py [-p|-c] -i <inputfile> -o <outputfile>")
+    sys.exit(2)
 
-numl.process.hitgraph.process_file(out, "/files2/scratch/wkliao/Fermi/x0123_seq_cnt.h5")
+  # output pytorch files, one graph per file
+  out = numl.core.out.PTOut(outputfile)
+
+  # output HDF5 files, one graph per group
+  # out = numl.core.out.H5Out(outputfile)
+
+  numl.process.hitgraph.process_file(out, inputfile, use_seq=use_seq, profile=profiling)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
