d015e77020c8e69fb146cd56f617a2314ae0bd7d
wkliao <wkliao@users.noreply.github.com>
2021-09-18 14:29:29 -0500

    add commane-line option -5 to output HDF5 files

--- example/process.py
+++ example/process.py
@@ -4,37 +4,41 @@ import numl, glob
 import getopt
 
 def main(argv):
-  profiling = False
-  use_seq = False
-  inputfile = ""
+  profiling  = False
+  use_seq    = False
+  inputfile  = ""
   outputfile = ""
+  output_h5  = False
   try:
-    opts, args = getopt.getopt(argv,"hpsi:o:",["ifile=","ofile="])
+    opts, args = getopt.getopt(argv,"hps5i:o:",["ifile=","ofile="])
   except getopt.GetoptError:
-    print("Usage: process.py [-p|-s] -i <inputfile> -o <outputfile>")
+    print("Usage: process.py [-p|-s|-5] -i <inputfile> -o <outputfile>")
     sys.exit(2)
   for opt, arg in opts:
     if opt == "-h":
-      print("Usage: process.py [-p|-s] -i <inputfile> -o <outputfile>")
+      print("Usage: process.py [-p|-s|-5] -i <inputfile> -o <outputfile>")
       sys.exit()
     elif opt in ("-i", "--ifile"):
       inputfile = arg
     elif opt in ("-o", "--ofile"):
       outputfile = arg
+    elif opt == "-5":
+      output_h5 = True
     elif opt == "-p":
       profiling = True
     elif opt == "-s":
       use_seq = True
 
   if inputfile == "" or outputfile == "":
-    print("Usage: process.py [-p|-c] -i <inputfile> -o <outputfile>")
+    print("Usage: process.py [-p|-s|-5] -i <inputfile> -o <outputfile>")
     sys.exit(2)
 
-  # output pytorch files, one graph per file
-  out = numl.core.out.PTOut(outputfile)
-
-  # output HDF5 files, one graph per group
-  # out = numl.core.out.H5Out(outputfile)
+  if output_h5:
+    # output HDF5 files, one graph per group, one file per MPI process
+    out = numl.core.out.H5Out(outputfile)
+  else:
+    # output pytorch files, one graph per file
+    out = numl.core.out.PTOut(outputfile)
 
   numl.process.hitgraph.process_file(out, inputfile, use_seq=use_seq, profile=profiling)
 
