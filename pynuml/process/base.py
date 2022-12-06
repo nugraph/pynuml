40bbc580aac287bb846cc46db13acc9aa3db1eaa
v hewes <vhewes@fnal.gov>
2022-12-05 20:41:13 -0600

    update processing base class

--- pynuml/process/base.py
+++ pynuml/process/base.py
@@ -1,8 +1,18 @@
 from abc import ABC
-from typing import Any
+from typing import Any, NoReturn, Tuple
+
+from ..io import File
 
 class ProcessorBase(ABC):
-	'''Base class for event processing'''
+    '''Base class for event processing'''
 
-    def __call__(self, evt: Any) -> Any:
-    	raise NotImplementedError
+    def __init__(self, file: File):
+        for group, keys in self.columns.items():
+            file.add_group(group, keys)
+
+    @property
+    def columns(self) -> Dict[str, List[str]]:
+        raise NotImplementedError
+
+    def __call__(self, evt: Any) -> Tuple[str, Any]:
+        raise NotImplementedError
